8257794f19d4735b53d85c60d14dee450fad8e12
rashwin88 <raviashwin87@gmail.com>
2026-01-27 18:38:22 +0530

    Refactor file processing models and enhance HE5 file handling
    - Renamed `FileMetadata` to `ComponentMetadata` and added `FullMetadata` for comprehensive metadata structure.
    - Updated `FileSource` to `FileDownloaderType` for clarity and modified related fields.
    - Refactored `FileLoaderHelper` to accept a file path instead of a file source.
    - Enhanced `HE5Helper` to construct a detailed metadata structure and added methods for dataset access.
    - Added design documentation for file handling and included an illustrative flowchart.

--- app/models/file_processing/sources.py
+++ app/models/file_processing/sources.py
@@ -2,12 +2,19 @@ from enum import Enum
 from pydantic import BaseModel, Field
 
 
-class FileSource(Enum):
+class FileDownloaderType(Enum):
+    """
+    Defines the type of file downloader to use
+    """
+
     GOOGLE_DRIVE = "google_drive"
-    LOCAL_FILE = "local_file"
     S3 = "s3"
-    DOWNLOAD_LINK = "downloader"
+    DOWNLOAD_LINK = "download_link"
 
 
 class FileSourceConfig(BaseModel):
+    """
+    Configuration for the local file path
+    """
+
     source_path: str = Field(description="The source of the file")
--- app/utils/files/he5_helper.py
+++ app/utils/files/he5_helper.py
@@ -1,8 +1,12 @@
 import h5py
+from pydantic import ValidationError
 from app.models.file_processing.sources import FileSource, FileSourceConfig
 from app.utils.files.file_loader_helper import FileLoaderHelper
 from typing import Dict, List, Any
-from app.models.file_processing.file_metadata_models import FileMetadata
+from app.models.file_processing.file_metadata_models import (
+    FullMetadata,
+    ComponentMetadata,
+)
 from h5py import Group, Dataset
 
 import logging
@@ -17,64 +21,95 @@ class HE5Helper:
     Operates on multiple file sources.
     """
 
-    def __init__(self, file_source: FileSource, file_source_config: FileSourceConfig):
+    def __init__(self, file_source_config: FileSourceConfig):
         """
         Class constructor for file loading
         """
 
         # Initialize the file loader helper
         logger.info(f"Initializing file loader helper for file source: {file_source}")
-        self.file_loader_helper = FileLoaderHelper(file_source)
-        self.file_source_config = file_source_config
-        self.file_metadata = self._get_file_metadata()
+        self.file_source_config: FileSourceConfig = file_source_config
 
-        # We may now use this to construct a master meta data object for the He5 file
-        # This will be the basis for all processing that happens.
-
-    def _get_file_metadata(self):
-        """
-        Gets all the required file metadata
-        """
-        logger.info(
-            f"Getting file metadata for file: {self.file_source_config.source_path}"
+        # The raw strcture in itself can be used to access the metadata and the actual content in the file.
+        self.raw_structure: h5py.File = h5py.File(
+            self.file_source_config.source_path, "r"
         )
-        file_metadata = h5py.File(self.file_source_config.source_path, "r")
-        return file_metadata
+        self.file_metadata: FullMetadata = self._construct_metadata_structure()
 
-    def _construct_metadata_structure(self) -> Dict[str, Any]:
+    def get_dataset(self, path: str) -> Any:
+        """
+        Given a path, returns the dataset at that path
+        """
+        # First check if the path is a dataset
+        if path not in self.file_metadata.components:
+            raise TypeError(f"Path {path} not found in the file")
+        if not isinstance(self.raw_structure[path], Dataset):
+            raise TypeError(f"Path {path} is not a dataset")
+        return self.raw_structure[path][()]
+
+    def _get_clean_attrs(self, key_path: str | None = None) -> Dict[str, Any]:
+        """
+        Gets clean attributes for a given key path
+        """
+        clean_attrs = {}
+        if key_path is None:
+            metadata = self.raw_structure
+        else:
+            metadata = self.raw_structure[key_path]
+        for k, v in metadata.attrs.items():
+            if isinstance(v, bytes):
+                clean_attrs[k] = v.decode("utf-8", errors="ignore")
+            else:
+                clean_attrs[k] = v
+        return clean_attrs
+
+    def _construct_metadata_structure(self) -> FullMetadata:
         """
 
         Constructs a metadata structure for the He5 file
         """
+        output = FullMetadata()
         metadata_paths = []
         metadata_structure = {}
-        # Populate
-        self.file_metadata.visit(lambda name: metadata_paths.append(name))
+
+        # First we need to get the root metadata
+
+        root_meta = ComponentMetadata()
+        root_meta.type = type(self.raw_structure)
+        root_meta.shape = None
+        root_meta.is_scalar = False
+        root_meta.file_attributes = self._get_clean_attrs()
+        output.root_metadata = root_meta
+
+        # Visit each key in the raw structure and add it to the metadata paths
+        self.raw_structure.visit(lambda name: metadata_paths.append(name))
+        output.components = metadata_paths
 
         # Now for each path build up the metdata structure
         for path in metadata_paths:
-            metadata = FileMetadata()
-            metadata.type = type(self.file_metadata[path])
+            metadata = ComponentMetadata()
+            metadata.type = type(self.raw_structure[path])
             metadata_structure[path] = metadata
-
             # Check if this is a dataset
-            if isinstance(self.file_metadata[path], Dataset):
-                metadata.shape = self.file_metadata[path].shape
+            if isinstance(self.raw_structure[path], Dataset):
+                metadata.shape = self.raw_structure[path].shape
                 # We can add a small check to see if it a tensor or a scalar
                 if metadata.shape == ():
                     metadata.is_scalar = True
                 else:
                     metadata.is_scalar = False
-        return metadata_structure
+            metadata.file_attributes = self._get_clean_attrs(path)
+            output.component_metadata[path] = metadata
+        return output
 
 
 ## Local testing
 
 if __name__ == "__main__":
+    import pprint
+
     file_source = FileSource.LOCAL_FILE
     file_source_config = FileSourceConfig(
         source_path="raw_files/Hyper/PRS_L2D_STD_20231229050902_20231229050907_0001.he5"
     )
-    he5_helper = HE5Helper(file_source, file_source_config)
-    print(he5_helper.file_metadata)
-    print(he5_helper._construct_metadata_structure())
+    he5_helper = HE5Helper(file_source_config)
