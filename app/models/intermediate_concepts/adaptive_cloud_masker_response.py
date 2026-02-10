1b058b38fc6cf4f00831e7e70e14e8f73314803e
rashwin88 <raviashwin87@gmail.com>
2026-02-10 17:48:58 +0530

    Add new ML model classes and update coverage metrics
    - Introduced the `MlModel` abstract class for machine learning models, providing a base for model configuration, training, and prediction methods.
    - Added the `B10AdaptiveCloudMasker` class implementing the adaptive cloud masking model using Gaussian Mixture Models.
    - Created the `AdaptiveCloudMaskerResponse` class to define the response structure for the adaptive cloud masker.
    - Updated `coverage.xml` to reflect new coverage metrics, including additional lines covered in the newly added classes.
    - Added tests for the `B10AdaptiveCloudMasker` to ensure functionality and performance with real data inputs.

---
+++ app/models/intermediate_concepts/adaptive_cloud_masker_response.py
@@ -0,0 +1,28 @@
+"""
+Defines a model for the adaptive cloud masker response
+"""
+
+from typing import Any
+from pydantic import BaseModel, Field, ConfigDict
+import numpy as np
+from sklearn.mixture import GaussianMixture
+
+
+class AdaptiveCloudMaskerResponse(BaseModel):
+    """
+    Defines the response of the Adaptive cloud masker
+    """
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    cloud_mask: np.ndarray = Field(
+        ..., description="The final cloud mask as a boolean array"
+    )
+    n_comp: int = Field(
+        ..., description="The number of components used in the Gaussian Mixture Model"
+    )
+    model: GaussianMixture = Field(..., Field="The gaussian mixture modek")
+    anchors: Any = Field(..., description="The percentile probes used in the model")
+    pixels_masked: int = Field(
+        ..., description="The total number of pixels masked by the model"
+    )
